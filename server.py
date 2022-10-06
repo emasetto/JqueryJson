8f0d3914ecc390ac6871b9a4c25e44cad80107a3
emasetto <edhercarvalho@hotmail.com>
2022-10-06 20:08:15 -0300

    mudanca

--- server.py
+++ server.py
@@ -8,13 +8,13 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/teste', methods = ['POST'])
+@app.route('/corinthians', methods = ['POST'])
 def serialize():
-    pri_nome = request.form['_first_name']
-    ult_nome = request.form['_last_name']
+    nome = request.form['_first_name']
+    time = request.form['_last_name']
     posicao = request.form['_posicao']
-    return jsonify(_first_name = pri_nome.upper(), _last_name = ult_nome.upper(), _posicao = posicao.upper())
+    return jsonify(_first_name = nome.upper(), _last_name = time.upper(), _posicao = posicao.upper())
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5095))
+    port = int(os.environ.get('PORT', 5008))
     app.run(host = '0.0.0.0', port = port )
